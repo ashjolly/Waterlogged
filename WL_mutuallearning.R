bceff9643712dc9cfad25bc221e756b4a8bf9085
ashjolly <ashlee.jollymore@gmail.com>
2016-08-23 20:44:26 -0700

    git ignore for files that aren't relevant to analysis

---
+++ WL_mutuallearning.R
@@ -0,0 +1,23 @@
+# Waterlogged Script
+# Script for looking at Waterlogged Data
+# Aims:
+# 1. Statistically compare data collected by scientist to citizens - are they significantly different?
+# 2. Plot distributions of scientist data versus citizen data
+# 3. Plot distribution of scientist versus citizen data WITH specific points showing outliers/story points
+# Started 26April2016
+# Ashlee Jollymore's PhD work
+################################
+# clean up workspace
+
+# load data - 
+# our dataset (DBP)
+
+# citizen derived dataset
+
+
+##########
+# Aim 1 - is citizen data statistically different from scientist data?
+# Aim 2 - Plot distributions of both datasets
+# Aim 3 - PLot distributions with specific points
+
+
